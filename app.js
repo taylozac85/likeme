f4681d93ce75f9351d39aa5190ecffac724ce3dd
taylozac85 <taylozac85@gmail.com>
2013-06-07 16:02:32 -0700

    Switch app.js

--- app.js
+++ app.js
@@ -6,14 +6,25 @@ var express = require('express')
 
 var app = express();
 
-// Set up a URL route
-app.get("/", function(req, res) {
- res.send("Heroku Demo!");
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
 });
 
-// bind the app to listen for connections on a specified port
-var port = process.env.PORT || 3000;
-app.listen(port);
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
 
-// Render some console log output
-console.log("Listening on port " + port);
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
