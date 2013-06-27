751b8e7853e34bb5ca270b5375a78c22c1f69968
Nick Taylor <nick.taylor1207@gmail.com>
2013-06-26 18:22:09 -0700

    Update index routes

--- routes/index.js
+++ routes/index.js
@@ -1,36 +1,13 @@
+
 module.exports = function(app) {
 
-/*
- *  gets the user profile and loads users picture, title, discussions
- *  that they have been apart of, name, location, bio
- */
+	app.get('/', function(req, res) {
+		res.render('landing-page')
+	});
 
-app.get('/profile', function (req, res){
-  res.render('profile');
-});
+	app.get('/landing-page', function (req, res){
+	  res.render('landing-page');
+	});
 
-
-/*
- *  gets my-profile which is going to be the same route with an
- *	if statement that checks if it's the usesr and displays the edit button
- */ 
-
-app.get('/my-profile', function (req, res){
-  res.render('my-profile');
-});
-
-// get photos route based on user
-
-// get edit profile, it needs to load the data into each of the form fields
-
-
-app.get('/edit-profile', function (req, res){
-  res.render('edit-profile');
-});
-
-
-// update edit profile lets you modify the database and then redirects to get edit profile
-
-// remove photos allows you to delete photos
 
 };
