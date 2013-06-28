5918c9214d728bc80baa91111359de7fd7560677
taylozac85 <taylozac85@gmail.com>
2013-06-27 17:55:30 -0700

    Add view photo modal logic

--- public/javascripts/message.js
+++ public/javascripts/message.js
@@ -1,5 +1,30 @@
 $(function(){
-    $('#view-right-imgs-container').on('click', function(){
-        alert("hey mambo!");
+    
+    var $focusPic = $('#view-large-img');
+    
+    $('img.view-right-img').on('click', function(){
+        var $newPic = $(this);
+        var newSource = $newPic.attr("src");
+        $focusPic.attr("src", newSource);
+
+        $('img').remove('.view-bottom-img');
     });
+    
+    $('img.view-bottom-img').on('click', function(){
+        var $newPic = $(this);
+        var newSource = $newPic.attr("src");
+        var focusSource = $focusPic.attr("src");
+        $focusPic.attr("src", newSource);
+        $newPic.attr("src", focusSource);
+    });
+
+    $('img.home-img').on('click', function(){
+    	var $selectedPic = $(this);
+    	$focusPic.attr("src", $selectedPic.attr("src"));
+
+    	if ($selectedPic.attr("src") != "images/yoga2.jpg") {
+   			$('img').remove('.view-bottom-img');
+    	}
+    })
+
 });
